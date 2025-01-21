74a849464341f57325563ae439ff2bbb040d04e7
Yuvraj kumar <115492314+yuvrajkumar12@users.noreply.github.com>
2025-01-22 00:18:59 +0530

    update

---
+++ src/componentes/Header/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./style.css";
+function Header() {
+  return (
+    <div className="navbar">header</div>
+  )
+}
+
+export default Header;
---
+++ src/pages/signup.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function signup() {
+  return (
+    <div>signup page</div>
+  )
+}
+
+export default signup
