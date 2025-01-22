bee21a8606f8647207c5b8615597dc459ce3ad7c
Yuvraj kumar <115492314+yuvrajkumar12@users.noreply.github.com>
2025-01-23 03:38:14 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import Header from './componentes/Header';
+import {BrowserRouter as Router,Route,Routes } from 'react-router-dom';
 function App() {
   return (
-    <div className="App">
-      <Header/>
-    </div>
+   <Router>
+    
+   </Router>
   );
 }
 
