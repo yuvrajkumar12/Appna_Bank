c6e5c1e30aa4e1b0f0e436e9e26007ab2e97e0f1
Yuvraj kumar <115492314+yuvrajkumar12@users.noreply.github.com>
2025-06-19 11:50:46 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,20 +1,43 @@
+// import "./App.css";
+// import Header from "./componentes/Header"; // Corrected the spelling of "componentes" to "components"
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import Signup from "./pages/signup";
+// import Dashboard from "./pages/Dashboard";
+// import { ToastContainer, toast } from "react-toastify";
+// import "react-toastify/dist/ReactToastify.css";
+// function App() {
+//   return (
+//     <>
+//       <ToastContainer/>
+//         <Router basename="/Appna_Bank">
+//           <Routes>
+//             <Route path="/" element={<Signup />} />
+//             <Route path="/dashboard" element={<Dashboard />} />
+//           </Routes>
+//         </Router>
+//     </>
+//   );
+// }
+
+// export default App;
 import "./App.css";
-import Header from "./componentes/Header"; // Corrected the spelling of "componentes" to "components"
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./componentes/Header"; // corrected folder name
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Signup from "./pages/signup";
 import Dashboard from "./pages/Dashboard";
-import { ToastContainer, toast } from "react-toastify";
+import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+
 function App() {
   return (
     <>
-      <ToastContainer/>
-        <Router basename="/Appna_Bank">
-          <Routes>
-            <Route path="/" element={<Signup />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-          </Routes>
-        </Router>
+      <ToastContainer />
+      <Router basename="/Appna_Bank">
+        <Routes>
+          <Route path="/" element={<Signup />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </Router>
     </>
   );
 }
